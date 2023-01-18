23bd210c07624c415437566ae09beb6713c8bf65
zihuaLi <13364064743@163.com>
2023-01-18 13:41:47 +0100

    change

--- authentication/addgroup.php
+++ authentication/addgroup.php
@@ -1,4 +1,29 @@
 <?php
-$groupname=$_REQUEST[''];
+include "db.php";
+if (isset($_POST['submit'])) {
+    $groupname=$_POST['groupname'];
+    $device="";
+    $devices = $_POST['devices'];
+    $nSelection = count($devices);
+    for($i=0; $i < $nSelection; $i++)
+    {
+        $numberVal = $devices[$i];
 
+            if ($numberVal == "11"){
+            echo("Eleven"); 
+            }
+            else if ($numberVal == "12"){
+            echo("Twelve"); 
+            } 
+
+        }
+?>
+
+    $conn = mysqli_connect ("localhost","root", "","smarthome") ;
+
+    $sql = "select * from user where email = '$email' and password_hash = '$password'";  
+    $result = mysqli_query($conn, $sql);  
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+    $count = mysqli_num_rows($result);     
+}
 ?>
