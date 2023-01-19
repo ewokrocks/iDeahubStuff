09be2d31b9365003f940157b7102088c323c719e
XUYiteng123 <yiteng.xu@eleve.isep.fr>
2023-01-19 14:48:06 +0100

    validation about email and blank

--- core/contact.php
+++ core/contact.php
@@ -7,6 +7,7 @@ if (isset($_POST['contact'])) {
     $type = $_POST["type"];
     $message = $_POST["message"];
 
+
     $host = "localhost";
     $dbname = "smarthome";
     $username = "root";
@@ -43,8 +44,8 @@ if (isset($_POST['contact'])) {
     <?php
 
     require_once __DIR__ . '/../templates/navbar.php';
-
     ?>
+
 
     <div class="main">
 
@@ -54,20 +55,23 @@ if (isset($_POST['contact'])) {
             <div class="title_bottom">CONTACT US</div>
         </div>
 
-        <form action="../core/contact.php" method="post">
+        <form name = "contactpage" action="../core/contact.php" method="post" >
             <div class="content">
 
                 <div class="name"><label for="name">Name</label><br>
                     <input type="text" name="name" value="" maxlength="20" id="name">
+                    <small class = "error-message"></small>
                 </div>
 
                 <div class="surname"><label for="surname">Surname</label><br>
                     <input type="text" name="surname" value="" maxlength="20" id="surname">
+                    <small class = "error-message"></small>
                 </div>
 
                 <div class="contact">
                     <lable for="contact">E-mail</lable><br>
-                    <input type="text" name="email" value="" maxlength="70" id="contact">
+                    <input type="text" name="email" value="" maxlength="70" id="contact" onclick="ValidateEmail(document.form1.text1)">
+                    <small class = "error-message"></small>
                 </div>
 
                 <div class="catagory"><label for="category">Question Type</label><br>
@@ -90,16 +94,80 @@ if (isset($_POST['contact'])) {
                 </div>
                 
             </div> -->
+            
+            <!-- <script>
+            document.getElementById("contact").onblur = function () {
+            var reg = /^[0-9a-zA-Z_.-]+[@][0-9a-zA-Z_.-]+([.][a-zA-Z]+){1,2}$/;
+            if (reg.test(this.value)) {
+            this.style.backgroundColor = "#FCEDDA";
+            } else {
+            this.style.backgroundColor = "red";
+            alert("the format of e-mail is wrong!")
+            }
+            };
+            </script> -->
 
                 <div class="button">
-                    <div class="submit"><input type="submit" name="contact" value="Submit"></div>
+                    <div class="submit"><input type="submit" name="contact" value="submit" id = "submit"></div>
                     <div class="reset"><input type="reset" value="Reset"></div>
                 </div>
 
             </div>
         </form>
     </div>
+    <script>
+        const name = document.getElementById('name');
+        const surname = document.getElementById('surname');
+        const contact = document.getElementById('contact');
 
+        document.querySlector('contactpage').addEventListener('submit', e => {
+            e.preventDefault();
+            validateForm();
+        });
+
+        function printError(input, message){
+            const formControl = input.parentElement;
+            const errorMessage = formControl.querySelector('.error-message');
+            formControl.classList.add('error');
+            errorMessage.textContent = message;
+        }
+
+        function removeError(input) {
+            const formControl = input.parentElement;
+            formControl.classList.remove('error');
+        }
+
+        function validateEmail(email){
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+        }
+
+        function validateForm(){
+            const nameValue = name.value.trim();
+            const surnameValue = surname.value.trim();
+            const contactValue = contact.value.trim();
+
+            if (nameValue === ""){
+                printError(name, "please enter your name");
+            } else {
+                removeError(name);
+            }
+
+            if (surnameValue === ""){
+                printError(surname, "please enter your surname");
+            } else {
+                removeError(surname);
+            }
+
+            if (contactValue === ""){
+                printError(contact, "please enter your email");
+            } else if(!validateEmail(contactValue)){
+                printErroe(contact, 'the format of email is incorrect!');
+            } else{
+                removeError;
+            }
+        }
+        
+    </script>
 </body>
 
 </html>
