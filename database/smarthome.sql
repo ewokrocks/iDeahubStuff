8e31dd93d48a6192b6b476d7b85923aed35bf7d5
Hanyue FAN <hanyue.fan@eleve.isep.fr>
2023-01-29 00:41:17 +0100

    fix some mysql code

--- database/smarthome.sql
+++ database/smarthome.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE if not exists `smarthome`;
 use smarthome;
 
-CREATE TABLE `User`(
+CREATE TABLE if not exists `User`(
 `UserID` INT primary KEY,
 `FirstName` varchar(45) NOT NULL,
 `LastName` varchar(45) not null,
@@ -9,21 +9,21 @@ CREATE TABLE `User`(
 `Email` varchar(45) not null
 );
 
-CREATE TABLE `Device`(
+CREATE TABLE if not exists `Device`(
 `DeviceID` INT NOT NULL primary KEY,
 `DeviceName` varchar(45) not null,
 `DeviceStatus` tinyint,
 `UserID` INT references User(UserID)
 );
 
-CREATE TABLE `Group`(
+CREATE TABLE if not exists `Group`(
 `GroupID` INT primary key,
 `GroupName` varchar(45) not null,
 `UserID` int references User(UserID),
 `DeviceID` int references Device(DeviceID)
 );
 
-CREATE TABLE `DeviceData`(
+CREATE TABLE if not exists `DeviceData`(
 `DeviceDatald` int primary key,
 `Comsumption_type` varchar(45) not null,
 `Data` datetime not null,
@@ -31,15 +31,15 @@ CREATE TABLE `DeviceData`(
 `Device_ID` int references Device(DeviceID)
 );
 
-CREATE TABLE `DeviceVersion`(
+CREATE TABLE if not exists `DeviceVersion`(
 `idDeviceVersion` int primary key,
 `Title` varchar(45),
 `Value` int,
 `DeviceID` int references Device(DeviceID)
 );
 
-CREATE TABLE `message`(
-    `id` int auto_increasing,
+CREATE TABLE if not exists `message`(
+    `id` int primary key auto_increment,
     `name` varchar(128),
     `surname` varchar(128),
     `email` varchar(128),
@@ -47,7 +47,19 @@ CREATE TABLE `message`(
     `message` varchar(128)
 );
 
-alter table Group add column `image`;
+alter table `Group` add column if not exists `image` varchar(128);
+
+alter table `User` change UserID UserID INT auto_increment not null;
+
+alter table `Device` change DeviceID DeviceID int auto_increment not null;
+
+alter table `Group` change GroupID GroupID int auto_increment not null;
+
+alter table `DeviceData` change DeviceDatald DeviceDatald int auto_increment not null;
+
+alter table `DeviceVersion` change idDeviceVersion idDeviceVersion int auto_increment not null;
+
+
 
 
 
